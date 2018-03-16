021ebebc0691b7c13bc0562e66204cbc75ff6e9c
Gazelle Bahramian <gazellebahramian@gmail.com>
2018-03-15 21:20:15 -0700

    Social Analytics Homework

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "jy5gVE4OAByPWWTZL6tFp8CtX"
+consumer_secret = "foqDmvGgaX34VrGbDnz7xlfVpCkTFr5l78RgywqguaU3m3rgSF"
+access_token = "969400010327523328-nKELrQFcIFTbAazfWYcqbGV4nQwQUCw"
+access_token_secret = "R34KjYlYUjqgcO6JQOHfGejSAwKVug3AEu1q2n8wXpJdA"
